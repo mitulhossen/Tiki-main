4de8d7e621cf1d0fdf681d9c69dfd0cd44369f3f
Md. Mitul Hossen <143118872+mitulhossen@users.noreply.github.com>
2023-12-26 23:10:29 +0600

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
          \App\Models\User::factory()->create([
-             'name' => 'Mahady Hasan Sabuz',
+             'name' => 'Mitul Hossen',
              'email' => 'admin@gmail.com',
              'password' => Hash::make("12345")
          ]);
